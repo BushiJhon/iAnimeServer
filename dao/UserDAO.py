08655d10317f10d9377a8b3e23235281bc9a7d1a
Kind-Jhon <1172905107@qq.com>
2019-11-11 17:00:08 +0800

    The User DAO

--- dao/UserDAO.py
+++ dao/UserDAO.py
@@ -8,11 +8,10 @@ class UserDAO:
     __db_password = '5257107'
     __db = 'iAnimeSQL'
 
-    # 注册操作
-    def login(self, user):
+    # 执行SQL语句
+    def __execute_SQL(self, sql):
         db = pymysql.connect(self.__db_host, self.__db_admin, self.__db_password, self.__db)
         cursor = db.cursor()
-        sql = 'insert into user(user_id, phone, password) values( null, %s, "%s" )'%(user.get_phone(), user.get_password())
         try:
             cursor.execute(sql)
             db.commit()
@@ -20,4 +19,16 @@ class UserDAO:
             traceback.print_exc()
             db.rollback()
         db.close()
+
+    # 增加用户
+    def add(self, user):
+        sql = 'insert into user(user_id, phone, password) values( null, %s, "%s" )' % (user.get_phone(), user.get_password())
+        self.__execute_SQL(self, sql)
         return
+
+    # 查找
+    def retrieve(self, user):
+        sql = ''
+        self.__execute_SQL(self, sql)
+        return
+
