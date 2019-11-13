b49fa4ebce1ff1a1e45ce7454c2b339bdf78f28b
BushiJhon <1172905107@qq.com>
2019-11-13 23:55:39 +0800

    the Auth Class

--- auth/Auth.py
+++ auth/Auth.py
@@ -1,28 +1,43 @@
+from datetime import datetime, timedelta
+
 import jwt
-
-from dao.UserDAO import UserDAO
-from pojo.User import User
-
 
 class Auth:
     @staticmethod
-    def encode_auth_token():
-        return
+    def encode_auth_token(now_time, exp_time, id):
+        try:
+            payload = {
+                'exp': exp_time,
+                'iat': now_time,
+                'user_id': id
+            }
+            return jwt.encode(
+                payload,
+                'SECRET_KEY',
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
 
     @staticmethod
     def decode_auth_token():
+
         return
 
     # 用户登录授权
-    def authorize(self, phone, password):
-        # 搜索是否存在用户
-        user = User()
-        user.set_phone(phone)
-        user.set_password(password)
+    def authorize(self, user):
+        now_time = datetime.utcnow()
+        exp_time = now_time + timedelta(days=1)
 
-        user_dao = UserDAO()
-        #
-        return
+        Token = Auth.encode_auth_token(now_time, exp_time, user.get_user_id())
+        result = {
+            "StatusCode": 0,
+            "Token": Token.decode(),
+            "TokenExpire": exp_time
+        }
+        return result
 
     def identify(self):
         return
+
+Auth().decode_auth_token()
