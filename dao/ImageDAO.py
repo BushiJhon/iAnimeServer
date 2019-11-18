2c22a40e073b2b92aa5235821a091c6cc7544836
BushiJhon <1172905107@qq.com>
2019-11-18 18:04:17 +0800

    the Image DAO

--- dao/ImageDAO.py
+++ dao/ImageDAO.py
@@ -3,46 +3,22 @@ import traceback
 import pymysql
 
 from pojo.Image import Work
-
+from dao.DatabaseConfig import *
 
 class WorkDAO:
-    __db_host = '127.0.0.1'
-    __db_admin = 'root'
-    __db_password = 'root'
-    __db = 'iAnimeSQL'
+    __db_host = host
+    __db_admin = admin
+    __db_password = password
+    __db = database
+    __port = port
+    __charset = charset
 
-    def home_retrieve(self, artist):
+    def retrieve(self, artist):
         retrieve_works = []
 
-        sql = 'select * from work where artist = %s' % (artist)
-        connection = pymysql.connect(self.__db_host, self.__db_admin, self.__db_password, self.__db, charset='utf8')
-        cursor = connection.cursor()
-        try:
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            if result is not None:
-                for index in range(len(result)):
-                    work = result[index]
-                    result = {
-                        "id": work[0],
-                        "name": work[3],
-                        "created": work[4],
-                        "artist_name": work[2]
-                    }
-                    retrieve_works.append(result)
-        except:
-            traceback.print_exc()
-        finally:
-            connection.close()
-            cursor.close()
-
-        return retrieve_works
-
-    def detail_retrieve(self, artist):
-        retrieve_works = []
-
-        sql = 'select * from work where artist = %s' % (artist)
-        connection = pymysql.connect(self.__db_host, self.__db_admin, self.__db_password, self.__db, charset='utf8')
+        sql = 'select * from work where artist = %s' % artist
+        connection = pymysql.connect(host=self.__db_host, user=self.__db_admin, password=self.__db_password,
+                                     database=self.__db, port=self.__port, charset=self.__charset)
         cursor = connection.cursor()
         try:
             cursor.execute(sql)
@@ -56,27 +32,13 @@ class WorkDAO:
                     work.set_artist_name(work_result[2])
                     work.set_name(work_result[3])
                     work.set_created(work_result[4])
-                    work.set_allow_download(work_result[5])
+                    work.set_description(work_result[5])
                     work.set_forks(work_result[6])
                     work.set_likes(work_result[7])
-                    work.set_allow_download(work_result[8])
-                    work.set_allow_sketch(work_result[9])
-                    work.set_allow_fork(work_result[10])
-                    result = {
-                        "id": work.get_id(),
-                        "artist": work.get_artist(),
-                        "artist_name": work.get_artist_name(),
-                        "name": work.get_name(),
-                        "created": work.get_created(),
-                        "description": work.get_description(),
-                        "tags": work.get_tags(),
-                        "forks": work.get_forks(),
-                        "like": work.get_likes(),
-                        "allow_download": work.get_description(),
-                        "allow_sketch": work.get_allow_sketch(),
-                        "allow_fork": work.get_allow_fork()
-                    }
-                    retrieve_works.append(result)
+                    work.set_allow_download(bool(work_result[8]))
+                    work.set_allow_sketch(bool(work_result[9]))
+                    work.set_allow_fork(bool(work_result[10]))
+                    retrieve_works.append(work)
         except:
             traceback.print_exc()
         finally:
@@ -85,12 +47,60 @@ class WorkDAO:
 
         return retrieve_works
 
+    # def detail_retrieve(self, artist):
+    #     retrieve_works = []
+    #
+    #     sql = 'select * from work where artist = %s' % (artist)
+    #     connection = pymysql.connect(self.__db_host, self.__db_admin, self.__db_password, self.__db, charset='utf8')
+    #     cursor = connection.cursor()
+    #     try:
+    #         cursor.execute(sql)
+    #         result = cursor.fetchall()
+    #         if result is not None:
+    #             for index in range(len(result)):
+    #                 work_result = result[index]
+    #                 work = Work()
+    #                 work.set_id(work_result[0])
+    #                 work.set_artist(work_result[1])
+    #                 work.set_artist_name(work_result[2])
+    #                 work.set_name(work_result[3])
+    #                 work.set_created(work_result[4])
+    #                 work.set_allow_download(work_result[5])
+    #                 work.set_forks(work_result[6])
+    #                 work.set_likes(work_result[7])
+    #                 work.set_allow_download(work_result[8])
+    #                 work.set_allow_sketch(work_result[9])
+    #                 work.set_allow_fork(work_result[10])
+    #                 result = {
+    #                     "id": work.get_id(),
+    #                     "artist": work.get_artist(),
+    #                     "artist_name": work.get_artist_name(),
+    #                     "name": work.get_name(),
+    #                     "created": work.get_created(),
+    #                     "description": work.get_description(),
+    #                     "tags": work.get_tags(),
+    #                     "forks": work.get_forks(),
+    #                     "like": work.get_likes(),
+    #                     "allow_download": work.get_description(),
+    #                     "allow_sketch": work.get_allow_sketch(),
+    #                     "allow_fork": work.get_allow_fork()
+    #                 }
+    #                 retrieve_works.append(result)
+    #     except:
+    #         traceback.print_exc()
+    #     finally:
+    #         connection.close()
+    #         cursor.close()
+    #
+    #     return retrieve_works
+
     # 获取地址
     def retrieve_address(self, id):
         address = None
 
-        sql = 'select address from work where id = %s' % (id)
-        connection = pymysql.connect(self.__db_host, self.__db_admin, self.__db_password, self.__db, charset='utf8')
+        sql = 'select path from address where work_id = %s' % (id)
+        connection = pymysql.connect(host=self.__db_host, user=self.__db_admin, password=self.__db_password,
+                                     database=self.__db, port=self.__port, charset=self.__charset)
         cursor = connection.cursor()
         try:
             cursor.execute(sql)
@@ -108,12 +118,13 @@ class WorkDAO:
     def list(self, ids):
         my_like_works = []
 
-        connection = pymysql.connect(self.__db_host, self.__db_admin, self.__db_password, self.__db, charset='utf8')
+        connection = pymysql.connect(host=self.__db_host, user=self.__db_admin, password=self.__db_password,
+                                     database=self.__db, port=self.__port, charset=self.__charset)
         cursor = connection.cursor()
 
         try:
             for index in range(len(ids)):
-                sql = 'select * from work where id = %s' % (ids[index])
+                sql = 'select * from information where id = %s' % (ids[index])
                 cursor.execute(sql)
                 work_result = cursor.fetchone()
                 work = Work()
@@ -122,12 +133,12 @@ class WorkDAO:
                 work.set_artist_name(work_result[2])
                 work.set_name(work_result[3])
                 work.set_created(work_result[4])
-                work.set_allow_download(work_result[5])
+                work.set_description(work_result[5])
                 work.set_forks(work_result[6])
                 work.set_likes(work_result[7])
-                work.set_allow_download(work_result[8])
-                work.set_allow_sketch(work_result[9])
-                work.set_allow_fork(work_result[10])
+                work.set_allow_download(bool(work_result[8]))
+                work.set_allow_sketch(bool(work_result[9]))
+                work.set_allow_fork(bool(work_result[10]))
                 my_like_works.append(work)
         except:
             traceback.print_exc()
@@ -137,4 +148,34 @@ class WorkDAO:
 
         return my_like_works
 
+    def retrieve_information(self, id):
+        result_work = None
+
+        sql = 'select * from work where id = %s' % id
+        connection = pymysql.connect(host=self.__db_host, user=self.__db_admin, password=self.__db_password,
+                                     database=self.__db, port=self.__port, charset=self.__charset)
+        cursor = connection.cursor()
+        try:
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            if result is not None:
+                result_work = Work()
+                result_work.set_id(result[0])
+                result_work.set_artist(result[1])
+                result_work.set_artist_name(result[2])
+                result_work.set_name(result[3])
+                result_work.set_created(result[4])
+                result_work.set_description(result[5])
+                result_work.set_forks(result[6])
+                result_work.set_likes(result[7])
+                result_work.set_allow_download(bool(result[8]))
+                result_work.set_allow_sketch(bool(result[9]))
+                result_work.set_allow_fork(bool(result[10]))
+        except:
+            traceback.print_exc()
+        finally:
+            connection.close()
+            cursor.close()
+
+        return result_work
 
