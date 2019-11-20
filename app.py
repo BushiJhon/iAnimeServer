e5d650316bd350de8dd3c6f847ca05278ba7162b
BushiJhon <1172905107@qq.com>
2019-11-20 13:33:12 +0800

    the application router

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import base64
 import os
 import re
 import random
+import threading
+
 import cv2
 import numpy as np
 
@@ -19,7 +21,7 @@ from pojo.Image import Work
 from pojo.Information import Information
 from pojo.User import User
 from operation.tricks import *
-# from operation.ai import *
+from operation.ai import *
 
 
 from Result import *
@@ -717,6 +719,7 @@ def like():
         result = return_status(-1)
         return jsonify(result)
 
+
 # 获取作品详情
 @app.route('/illustration/sketchwork', methods=['GET'])
 def get_sketchwork():
@@ -871,56 +874,66 @@ def get_request_image(image):
     return image
 
 
-pool = []
+# pool = []
+# lock = 1
 #
 #
-# def handle_colorization():
-#     if len(pool) > 0:
-#         sketch, points, path = pool[0]
-#         del pool[0]
-#     improved_sketch = sketch.copy()
-#     improved_sketch = min_resize(improved_sketch, 512)
-#     improved_sketch = cv_denoise(improved_sketch)
-#     improved_sketch = sensitive(improved_sketch, s=5.0)
-#     improved_sketch = go_tail(improved_sketch)
-#
-#     std = cal_std(improved_sketch)
-#     if std > 100.0:
-#         improved_sketch = go_passline(improved_sketch)
-#         improved_sketch = min_k_down_c(improved_sketch, 2)
-#         improved_sketch = cv_denoise(improved_sketch)
-#         improved_sketch = go_tail(improved_sketch)
-#         improved_sketch = sensitive(improved_sketch, s=5.0)
-#
-#     improved_sketch = min_black(improved_sketch)
-#     improved_sketch = cv2.cvtColor(improved_sketch, cv2.COLOR_BGR2GRAY)
-#     sketch_1024 = k_resize(improved_sketch, 64)
-#     sketch_256 = mini_norm(k_resize(min_k_down(sketch_1024, 2), 16))
-#     sketch_128 = hard_norm(sk_resize(min_k_down(sketch_1024, 4), 32))
-#
-#     baby = go_baby(sketch_128, opreate_normal_hint(ini_hint(sketch_128), points, type=0, length=1))
-#     baby = de_line(baby, sketch_128)
-#     for _ in range(16):
-#         baby = blur_line(baby, sketch_128)
-#     baby = go_tail(baby)
-#     baby = clip_15(baby)
-#
-#     composition = go_gird(sketch=sketch_256, latent=d_resize(baby, sketch_256.shape), hint=ini_hint(sketch_256))
-#     composition = go_tail(composition)
-#
-#     painting_function = go_head
-#     reference = None
-#     alpha = 0
-#     result = painting_function(
-#         sketch=sketch_1024,
-#         global_hint=k_resize(composition, 14),
-#         local_hint=opreate_normal_hint(ini_hint(sketch_1024), points, type=2, length=2),
-#         global_hint_x=k_resize(reference, 14) if reference is not None else k_resize(composition, 14),
-#         alpha=(1 - alpha) if reference is not None else 1
-#     )
-#     result = go_tail(result)
-#     cv2.imwrite(path, result)
-#     return
+def handle_colorization(pool):
+    # mutex = threading.Lock()
+    # 锁定
+    # mutex.acquire(lock)
+    # print(len(pool))
+    # if len(pool) > 0:
+    #     sketch, points, path = pool[0]
+    #     del pool[0]
+    # else:
+    #     return
+    # 释放
+    # mutex.release()
+    sketch, points, path = pool
+    improved_sketch = sketch.copy()
+    improved_sketch = min_resize(improved_sketch, 512)
+    improved_sketch = cv_denoise(improved_sketch)
+    improved_sketch = sensitive(improved_sketch, s=5.0)
+    improved_sketch = go_tail(improved_sketch)
+
+    std = cal_std(improved_sketch)
+    if std > 100.0:
+        improved_sketch = go_passline(improved_sketch)
+        improved_sketch = min_k_down_c(improved_sketch, 2)
+        improved_sketch = cv_denoise(improved_sketch)
+        improved_sketch = go_tail(improved_sketch)
+        improved_sketch = sensitive(improved_sketch, s=5.0)
+
+    improved_sketch = min_black(improved_sketch)
+    improved_sketch = cv2.cvtColor(improved_sketch, cv2.COLOR_BGR2GRAY)
+    sketch_1024 = k_resize(improved_sketch, 64)
+    sketch_256 = mini_norm(k_resize(min_k_down(sketch_1024, 2), 16))
+    sketch_128 = hard_norm(sk_resize(min_k_down(sketch_1024, 4), 32))
+
+    baby = go_baby(sketch_128, opreate_normal_hint(ini_hint(sketch_128), points, type=0, length=1))
+    baby = de_line(baby, sketch_128)
+    for _ in range(16):
+        baby = blur_line(baby, sketch_128)
+    baby = go_tail(baby)
+    baby = clip_15(baby)
+
+    composition = go_gird(sketch=sketch_256, latent=d_resize(baby, sketch_256.shape), hint=ini_hint(sketch_256))
+    composition = go_tail(composition)
+
+    painting_function = go_head
+    reference = None
+    alpha = 0
+    result = painting_function(
+        sketch=sketch_1024,
+        global_hint=k_resize(composition, 14),
+        local_hint=opreate_normal_hint(ini_hint(sketch_1024), points, type=2, length=2),
+        global_hint_x=k_resize(reference, 14) if reference is not None else k_resize(composition, 14),
+        alpha=(1 - alpha) if reference is not None else 1
+    )
+    result = go_tail(result)
+    cv2.imwrite(path, result)
+    return
 
 
 # 提交上色请求
@@ -1001,8 +1014,9 @@ def colorization():
     address_dao.add(address)
 
     path = path + '/result.jpg'
-    pool.append([image, points, path])
-    cv2.imwrite(path, image)
+    pool = [image, points, path]
+    threading.Thread(target=handle_colorization, args=(pool, )).start()
+    # cv2.imwrite(path, image)
 
     result = return_receipt(0, address)
     return jsonify(result)
@@ -1044,5 +1058,14 @@ def get_receipt():
         return jsonify(result)
 
 
+# def handle_threading():
+#     while True:
+#         try:
+#             handle_colorization()
+#         except Exception as e:
+#             print(e)
+
+# threading.Thread(target=handle_threading).start()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, threaded=True)
